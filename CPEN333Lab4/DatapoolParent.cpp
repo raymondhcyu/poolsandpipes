6add9868ab6379f38e7fabd647e0fe2a57a94d9d
Raymond <yuraymond96@gmail.com>
2019-10-02 10:54:19 -0700

    Add project files.

---
+++ CPEN333Lab4/DatapoolParent.cpp
@@ -0,0 +1,45 @@
+# include "..\rt.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+struct myDatapoolData {
+	string name;
+	int EIS;
+	bool fixedWing;
+};
+
+int main() {
+	// Create datapool
+	cout << "Parent creating datapool..." << endl;
+	CDataPool dp("A320", sizeof(struct myDatapoolData));
+
+	// Link datapool
+	struct myDatapoolData* myDatapool = (struct myDatapoolData*)(dp.LinkDataPool());
+	cout << "Parent datapool linked at address " << myDatapool << endl;
+
+	// Write variables
+	myDatapool->name = "Airbus A320";
+	myDatapool->EIS = 1980;
+	myDatapool->fixedWing = true;
+
+	getchar();
+	// Create child processes to access data
+	CProcess p1("D:\\Documents\\CPEN333\\CPEN333Lab4\\Debug\\DataPoolChild.exe",	// pathlist to child program executable
+		NORMAL_PRIORITY_CLASS,			// priority
+		OWN_WINDOW,						// process has its own window
+		ACTIVE							// process is active immediately
+	);
+
+	CProcess p2("D:\\Documents\\CPEN333\\CPEN333Lab4\\Debug\\DataPoolChild2.exe",	// pathlist to child program executable
+		NORMAL_PRIORITY_CLASS,			// priority
+		OWN_WINDOW,						// process has its own window
+		ACTIVE							// process is active immediately
+	);
+
+	p1.WaitForProcess();
+	p2.WaitForProcess();
+	cout << "Parent complete..." << endl;
+	return 0;
+}
---
+++ DataPoolChild2/DataPoolChild2.cpp
@@ -0,0 +1,29 @@
+#include "..\rt.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+// Same struct as parent
+struct myDatapoolData {
+	string name;
+	int EIS;
+	bool fixedWing;
+};
+
+int main(void) {
+	cout << "Child process 2 creating/using datapool..." << endl;
+	CDataPool dp("A320", sizeof(struct myDatapoolData));
+
+	struct myDatapoolData* myDatapool = (struct myDatapoolData*)(dp.LinkDataPool());
+	cout << "Child datapool 2 linked at address " << myDatapool << endl;
+
+	// Show that child access datapool which parent wrote to
+	cout << "Name: " << myDatapool->name << endl;
+	cout << "Entry into service: " << myDatapool->EIS << endl;
+	cout << "Is it a fixed-wing? " << myDatapool->fixedWing << endl;
+
+	cout << "Child process 2 complete..." << endl;
+	getchar();
+	return 0;
+}
