d42b66bc368c27b6eb3af79193ad69d88970f974
Raymond <yuraymond96@gmail.com>
2019-10-02 21:45:01 -0700

    Part B complete functioning and verified

--- PartBParent/PartBParent.cpp
+++ PartBParent/PartBParent.cpp
@@ -1,5 +1,10 @@
 #include "..\rt.h"
 
+void PartBPolling();
+void PartBThreads();
+
+int pipe1Data, pipe2Data, pipe3Data, pipe4Data; // int is 4 bytes in C++
+
 struct Pipe1Struct {
 	int data10;
 	int data11;
@@ -20,10 +25,52 @@ struct Pipe4Struct {
 	int data41;
 };
 
-int pipe1Data, pipe2Data, pipe3Data, pipe4Data; // int is 4 bytes in C++
+UINT __stdcall Thread1(void* args) {
+	CTypedPipe <int> Pipe1("Pipeline1", 100);
+	while (1) {
+		if (Pipe1.TestForData() >= sizeof(pipe1Data) / 2) {
+			Pipe1.Read(&pipe1Data);
+			cout << "Parent read " << pipe1Data << " from Pipeline 1..." << endl;
+		}
+	}
+	return 0;
+}
 
-void PartBPolling();
-void PartBThreads();
+UINT __stdcall Thread2(void* args) {
+	CTypedPipe <int> Pipe2("Pipeline2", 100);
+
+	while (1) {
+		if (Pipe2.TestForData() >= sizeof(pipe2Data) / 2) {
+			Pipe2.Read(&pipe2Data);
+			cout << "Parent read " << pipe2Data << " from Pipeline 2..." << endl;
+		}
+	}
+	return 0;
+}
+
+UINT __stdcall Thread3(void* args) {
+	CTypedPipe <int> Pipe3("Pipeline3", 100);
+
+	while (1) {
+		if (Pipe3.TestForData() >= sizeof(pipe3Data) / 2) {
+			Pipe3.Read(&pipe3Data);
+			cout << "Parent read " << pipe3Data << " from Pipeline 3..." << endl;
+		}
+	}
+	return 0;
+}
+
+UINT __stdcall Thread4(void* args) {
+	CTypedPipe <int> Pipe4("Pipeline4", 100);
+
+	while (1) {
+		if (Pipe4.TestForData() >= sizeof(pipe4Data) / 2) {
+			Pipe4.Read(&pipe4Data);
+			cout << "Parent read " << pipe4Data << " from Pipeline 4..." << endl;
+		}
+	}
+	return 0;
+}
 
 int main(void) {
 
@@ -31,13 +78,13 @@ int main(void) {
 	PartBPolling();
 
 	/* PART B THREADS*/
-	PartBThreads();
+	//PartBThreads();
 
 	return 0;
 }
 
 void PartBPolling() {
-	cout << "Starting parent process..." << endl;
+	cout << "Starting parent process using polling..." << endl;
 
 	CTypedPipe <int> Pipe1("Pipeline1", 100);
 	CTypedPipe <int> Pipe2("Pipeline2", 100);
@@ -69,7 +116,7 @@ void PartBPolling() {
 	);
 
 	while (1) {
-		if (Pipe1.TestForData() >= (sizeof(pipe1Data) / 2)) {
+		if (Pipe1.TestForData() >= sizeof(pipe1Data) / 2) {
 			Pipe1.Read(&pipe1Data);
 			cout << "Parent read " << pipe1Data << " from Pipeline 1..." << endl;
 		}
@@ -86,6 +133,46 @@ void PartBPolling() {
 			cout << "Parent read " << pipe4Data << " from Pipeline 4..." << endl;
 		}
 	}
+
+	p1.WaitForProcess();
+	p2.WaitForProcess();
+	p3.WaitForProcess();
+	p4.WaitForProcess();
+}
+
+void PartBThreads() {
+	cout << "Starting parent process using threads..." << endl;
+
+	CThread t1(Thread1, ACTIVE, NULL);
+	CThread t2(Thread2, ACTIVE, NULL);
+	CThread t3(Thread3, ACTIVE, NULL);
+	CThread t4(Thread4, ACTIVE, NULL);
+
+	CProcess p1("D:\\Documents\\CPEN333\\CPEN333Lab4\\Debug\\PartBChild1.exe",
+		NORMAL_PRIORITY_CLASS,
+		OWN_WINDOW,
+		ACTIVE
+	);
+	CProcess p2("D:\\Documents\\CPEN333\\CPEN333Lab4\\Debug\\PartBChild2.exe",
+		NORMAL_PRIORITY_CLASS,
+		OWN_WINDOW,
+		ACTIVE
+	);
+	CProcess p3("D:\\Documents\\CPEN333\\CPEN333Lab4\\Debug\\PartBChild3.exe",
+		NORMAL_PRIORITY_CLASS,
+		OWN_WINDOW,
+		ACTIVE
+	);
+	CProcess p4("D:\\Documents\\CPEN333\\CPEN333Lab4\\Debug\\PartBChild4.exe",
+		NORMAL_PRIORITY_CLASS,
+		OWN_WINDOW,
+		ACTIVE
+	);
+
+	t1.WaitForThread();
+	t2.WaitForThread();
+	t3.WaitForThread();
+	t4.WaitForThread();
 
 	p1.WaitForProcess();
 	p2.WaitForProcess();
