780a652cfc61d3ba4e819eb706fd6ad8acd5297e
Raymond <yuraymond96@gmail.com>
2019-10-03 10:15:07 -0700

    Minor changes

--- CPEN333Lab4/DatapoolParent.cpp
+++ CPEN333Lab4/DatapoolParent.cpp
@@ -25,12 +25,14 @@ int main() {
 	myDatapool->fixedWing = true;
 
 	getchar();
+
 	// Create child processes to access data
 	CProcess p1("D:\\Documents\\CPEN333\\CPEN333Lab4\\Debug\\DataPoolChild.exe",	// pathlist to child program executable
 		NORMAL_PRIORITY_CLASS,			// priority
 		OWN_WINDOW,						// process has its own window
 		ACTIVE							// process is active immediately
 	);
+
 	CProcess p2("D:\\Documents\\CPEN333\\CPEN333Lab4\\Debug\\DataPoolChild2.exe",	// pathlist to child program executable
 		NORMAL_PRIORITY_CLASS,			// priority
 		OWN_WINDOW,						// process has its own window
